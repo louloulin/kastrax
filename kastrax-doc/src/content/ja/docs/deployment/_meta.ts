19b17c14c0016e6a4bbe11acbd6428baa66d4817
louloulin <729883852@qq.com>
2025-05-01 16:56:01 +0800

    删除jp

--- kastrax-doc/src/content/ja/docs/deployment/_meta.ts
+++
@@ -1,7 +0,0 @@
-const meta = {
-  overview: "概要",
-  server: "サーバーを使用",
-  deployment: "サーバーレスプラットフォーム",
-  client: "Mastraクライアント",
-};
-export default meta;
--- kastrax-doc/src/content/ja/docs/frameworks/_meta.ts
+++
@@ -1,6 +0,0 @@
-const meta = {
-  "ai-sdk": "Vercel AI SDKを使用",
-  "next-js": "Next.jsを使用",
-};
-
-export default meta;
