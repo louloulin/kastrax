b9493bdc38c28f4507117d5cb4df4651864e3c7e
louloulin <729883852@qq.com>
2025-05-01 16:51:14 +0800

    文档

---
+++ kastrax-doc/src/content/en/docs/_meta.ts
@@ -0,0 +1,49 @@
+const meta = {
+  index: "Introduction",
+  "getting-started": {
+    title: "Getting Started",
+  },
+  frameworks: {
+    title: "Frameworks",
+  },
+  agents: {
+    title: "Agents",
+  },
+  memory: {
+    title: "Memory",
+  },
+  workflows: {
+    title: "Workflows",
+  },
+  "workflows-vnext": {
+    title: "Workflows (vNext)",
+  },
+  rag: {
+    title: "RAG",
+  },
+  "local-dev": {
+    title: "Local Dev",
+  },
+  deployment: {
+    title: "Deployment",
+  },
+  "mastra-cloud": {
+    title: "Mastra Cloud",
+  },
+  observability: {
+    title: "Observability",
+  },
+  evals: {
+    title: "Evals",
+  },
+  storage: {
+    title: "Storage",
+  },
+  voice: {
+    title: "Voice",
+  },
+  integrations: "Integrations",
+  community: "Community",
+};
+
+export default meta;
